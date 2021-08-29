87bb154fd318f97d37a33d55e954586d12bb281d
Daniel Lamando <me@danopia.net>
2021-08-29 18:01:34 +0200

    Just put djwt on main whatever

--- integrations/google-service-account.ts
+++ integrations/google-service-account.ts
@@ -1,4 +1,4 @@
-import * as JWT from "https://raw.githubusercontent.com/cloudydeno/djwt/11c50daa9d831b72d424ad15652642ca2175e6d5/mod.ts";
+import * as JWT from "https://raw.githubusercontent.com/cloudydeno/pure_djwt/main/mod.ts";
 
 export const _mockCurrentTime = Symbol();
 export const _mockFetch = Symbol();
