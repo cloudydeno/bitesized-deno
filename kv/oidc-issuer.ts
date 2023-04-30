92041a22d4157a2551504def2efbd9a318607eaa
Daniel Lamando <dan@danopia.net>
2023-04-30 23:31:37 +0200

    Fix up kv/oidc-issuer.ts

--- kv/oidc-issuer.ts
+++ kv/oidc-issuer.ts
@@ -1,127 +1,151 @@
-import * as JWT from "https://deno.land/x/djwt@v2.8/mod.ts";
+import {
+  type Header,
+  type Payload,
+  create as createJwt,
+  getNumericDate,
+} from "https://deno.land/x/djwt@v2.8/mod.ts";
+export { getNumericDate };
+
+type StoredKey = {
+  header: {
+    kid: string;
+    alg: Header['alg'];
+  };
+  publicJwk: JsonWebKey;
+  privateJwk: JsonWebKey;
+  algorithm: KeyAlgorithm;
+}
 
 export class OidcIssuer {
-    constructor(
-        private readonly kv: Deno.Kv,
-        public readonly kvPrefix: string[],
-        public readonly keyOptions: RsaHashedKeyGenParams | EcKeyGenParams,
-    ) {}
-    
-    async getCurrentPrivateKey() {
-        const foundKey = await this.getLatestPrivateKey();
-        return foundKey ?? await this.issueNewKey();
+  constructor(
+    private readonly kv: Deno.Kv,
+    public readonly kvPrefix: string[],
+    public readonly keyGenOptions: RsaHashedKeyGenParams | EcKeyGenParams,
+  ) {}
+
+  async getCurrentPrivateKey() {
+    const foundKey = await this.getLatestPrivateKeyIfAny();
+    return foundKey ?? await this.issueNewKey();
+  }
+
+  async getCurrentKeys() {
+    const keys: Array<JsonWebKey & { kid: string }> = [];
+    const oldestReasonable = Date.now() - (1000 * 60 * 60 * 24); // one day old
+    const iter = this.kv.list({
+      prefix: [...this.kvPrefix],
+      start: [...this.kvPrefix, oldestReasonable],
+    }, {
+      // Rationale: we need to include just-created keys
+      // consistency: 'eventual',
+    });
+    for await (const res of iter) {
+      const record = res.value as StoredKey;
+      keys.push({
+        kid: record.header.kid,
+        ...record.publicJwk,
+        use: 'sig',
+        key_ops: undefined,
+        ext: undefined,
+      });
     }
+    return keys;
+  }
 
-    async getCurrentKeys() {
-        const keys = [];
-        const oldestReasonable = Date.now() - (1000 * 60 * 60 * 24); // one day old
-        const iter = this.kv.list({
-            prefix: [...this.kvPrefix],
-            start: [...this.kvPrefix, oldestReasonable],
-        }, {
-            // consistency: 'eventual',
-        });
-        for await (const res of iter) {
-            keys.push({
-                kid: res.value.header.kid,
-                ...res.value.publicJwk,
-                use: 'sig',
-                key_ops: undefined,
-                ext: undefined,
-            });
-        }
-        return keys;
+  async dropOldKeys() {
+    const oldestReasonable = Date.now() - ((1000 * 60 * 60) * 24); // one day old
+    const mutations = new Array<Deno.KvMutation>();
+    const iter = this.kv.list({
+      prefix: [...this.kvPrefix],
+      end: [...this.kvPrefix, oldestReasonable],
+    }, {
+      // rationale: latency hit not important
+      // consistency: 'eventual',
+    });
+    for await (const res of iter) {
+      mutations.push({
+        key: res.key,
+        type: "delete",
+      });
     }
-
-    async dropOldKeys() {
-        const oldestReasonable = Date.now() - ((1000 * 60 * 60) * 24); // one day old
-        const oldKeys = [];
-        const iter = this.kv.list({
-            prefix: [...this.kvPrefix],
-            end: [...this.kvPrefix, oldestReasonable],
-        }, {
-            // consistency: 'eventual',
-        });
-        for await (const res of iter) {
-            oldKeys.push(res.key);
-        }
-        if (oldKeys.length) {
-            await kv.atomic()
-                .mutate(...oldKeys.map(key => ({
-                    key,
-                    type: "delete",
-                })))
-                .commit();
-        }
-        console.log('Dropped', oldKeys.length, 'old signing keys');
+    if (mutations.length) {
+      await this.kv.atomic()
+        .mutate(...mutations)
+        .commit();
     }
+    console.log('Dropped', mutations.length, 'old signing keys');
+  }
 
-    async getLatestPrivateKey() {
-        const oldestReasonable = Date.now() - ((1000 * 60 * 60) * 12); // some hours old
-        const iter = await this.kv.list({
-            prefix: [...this.kvPrefix],
-            start: [...this.kvPrefix, oldestReasonable],
-        }, {
-            reverse: true,
-            limit: 1,
-            // consistency: 'eventual',
-        });
-        for await (const res of iter) {
-            console.log('found private key:', res.value.header);
-            const cryptoKey = await crypto.subtle.importKey(
-                'jwk',
-                res.value.privateJwk,
-                res.value.algorithm,
-                false,
-                ['sign'],
-            );
-            return {
-                header: res.value.header,
-                privateKey: cryptoKey,
-            };
-        }
+  async getLatestPrivateKeyIfAny() {
+    const oldestReasonable = Date.now() - ((1000 * 60 * 60) * 12); // some hours old
+    const iter = await this.kv.list({
+      prefix: [...this.kvPrefix],
+      start: [...this.kvPrefix, oldestReasonable],
+    }, {
+      reverse: true,
+      limit: 1,
+      // rationale: reduce chances of creating multiple keys in parallel
+      // consistency: 'eventual',
+    });
+    for await (const res of iter) {
+      const record = res.value as StoredKey;
+      // console.log('found existing private key:', record.header);
+      const cryptoKey = await crypto.subtle.importKey(
+        'jwk',
+        record.privateJwk,
+        record.algorithm,
+        false,
+        ['sign'],
+      );
+      return {
+        header: record.header,
+        privateKey: cryptoKey,
+      };
     }
+    return null; // No valid key
+  }
 
-    async issueNewKey() {
-        console.log('making new key');
+  // TODO: this can easily create numerous keys per renewal if called frequently enough
+  async issueNewKey() {
+    // console.log('making new key');
 
-        // seems like this can take 50-100 ms
-        const key = await crypto.subtle
-            .generateKey(this.keyOptions, true, ["sign", "verify"]);
+    // seems like this can take 50-100 ms
+    const key = await crypto.subtle
+    .generateKey(this.keyGenOptions, true, ["sign", "verify"]);
 
-        const publicJwk = await crypto.subtle.exportKey('jwk', key.publicKey);
-        const privateJwk = await crypto.subtle.exportKey('jwk', key.privateKey);
-        const header = {
-            kid: (publicJwk.n ?? publicJwk.x).slice(0, 8),
-            alg: publicJwk.alg, // key.privateKey.algorithm.name.slice(0,2) + key.privateKey.algorithm.hash.name.slice(4),
-        };
+    const publicJwk = await crypto.subtle.exportKey('jwk', key.publicKey);
+    const privateJwk = await crypto.subtle.exportKey('jwk', key.privateKey);
 
-        // store key, make sure we never overwrite an existing key
-        const keyStamp = Date.now();
-        await kv.atomic()
-            .check({ key: [...this.kvPrefix, keyStamp], versionstamp: null })
-            .set([...this.kvPrefix, keyStamp], {
-                header,
-                publicJwk,
-                privateJwk,
-                algorithm: key.privateKey.algorithm,
-            })
-            .commit();
+    const newKey: StoredKey = {
+      header: {
+        kid: (publicJwk.n ?? publicJwk.x ?? Math.random().toString(16).slice(2)).slice(0, 8),
+        alg: publicJwk.alg as Header['alg'],
+      },
+      publicJwk,
+      privateJwk,
+      algorithm: key.privateKey.algorithm,
+    };
 
-        return {
-            header,
-            privateKey: key.privateKey,
-        };
-    }
+    // store key, make sure we never overwrite an existing key
+    const keyStamp = Date.now();
+    await this.kv.atomic()
+    .check({ key: [...this.kvPrefix, keyStamp], versionstamp: null })
+    .set([...this.kvPrefix, keyStamp], newKey)
+    .commit();
 
-    async signJwt(claims) {
-        const signingKey = await this.getCurrentPrivateKey();
-        const jwt = await JWT.create(signingKey.header, {
-            "exp": getNumericDate(5 * 60),
-            "iat": getNumericDate(0),
-            "nbf": getNumericDate(-5),
-            ...claims,
-        }, signingKey.privateKey);
-        return jwt;
-    }
+    return {
+      header: newKey.header,
+      privateKey: key.privateKey,
+    };
+  }
+
+  async signJwt(claims: Payload) {
+    const signingKey = await this.getCurrentPrivateKey();
+    const jwt = await createJwt(signingKey.header, {
+      "exp": getNumericDate(5 * 60),
+      "iat": getNumericDate(0),
+      "nbf": getNumericDate(-5),
+      ...claims,
+    }, signingKey.privateKey);
+    return jwt;
+  }
 }
